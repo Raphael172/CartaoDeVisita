390fc8745719177889b6f9001faa89a86dbed226
Raphael172 <raphael.rbezerra@hotmail.com>
2023-10-06 15:24:10 -0300

    A

--- app/src/main/java/com/example/cartaodevisita/MainActivity.kt
+++ app/src/main/java/com/example/cartaodevisita/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,6 +12,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
@@ -27,12 +29,12 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             CartaoDeVisitaTheme {
+                CartaoVisita()
                 }
             }
         }
     }
-@Preview(backgroundColor = long)
-@Preview(showSystemUi = true)
+@Preview(showSystemUi = true, showBackground = true, backgroundColor = 567)
 @Composable
 fun CartaoVisita() {
     Box {
@@ -41,30 +43,66 @@ fun CartaoVisita() {
             painter = painterResource(id = R.drawable.android_logo),
             contentDescription = null,
             contentScale = ContentScale.Fit,
-            modifier = Modifier.fillMaxSize()
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 100.dp)
         )
-        Column {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+
+
+
+        ) {
 
             Text(
-                text = "Full Name",
-                fontSize = 35.sp,
+                text = "Raphael Rodrigues",
+                fontSize = 45.sp,
+                fontWeight = FontWeight.Light,
+                color = Color.White,
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 420.dp))
+
+            Text(
+                text = "Android Developer Extraordinaire",
+                fontSize = 15.sp,
                 fontWeight = FontWeight.Black,
                 color = Color.White,
                 textAlign = TextAlign.Center,
-                        modifier = Modifier
-                        .fillMaxWidth()
-                    .padding(top = 525.dp)
-            )
-            Text(
-                text = "Title",
-                fontSize = 20.sp,
-                textAlign = TextAlign.Center,
-                fontWeight = FontWeight.Black,
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(end = 20.dp)
+                    .padding(top = 0.dp)
             )
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+
+            ) {
+                Contatos(text = "          +55 (11)95206-2629 ", Color.White)
+                Contatos(text = "@Rapha0riginal", Color.White)
+                Contatos(text = "                       Raphael.rb172@gmail.com", Color.White)
+            }
+            }
         }
     }
-}
+    @Composable
+    fun Contatos(text: String, color: Color) {
+
+        Text(
+            text = text,
+            fontSize = 17.sp,
+            color = color,
+            fontWeight = FontWeight.Black,
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 10.dp)
+                .padding(end = 30.dp)
+        )
+    }
+
 
