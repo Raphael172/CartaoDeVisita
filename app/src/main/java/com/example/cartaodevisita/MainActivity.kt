21967023a3bb22974f7287aae55f41533735e24e
Raphael172 <raphael.rbezerra@hotmail.com>
2023-10-11 13:37:56 -0300

    Finalizadoo

--- app/src/main/java/com/example/cartaodevisita/MainActivity.kt
+++ app/src/main/java/com/example/cartaodevisita/MainActivity.kt
@@ -4,13 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -21,7 +24,6 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -49,58 +51,19 @@ fun CartaoVisita() {
             modifier = Modifier
                 .fillMaxSize()
         )
-        Box {
 
-            Image(
-                painter = painterResource(id = R.drawable.android_logo),
-                contentDescription = null,
-                contentScale = ContentScale.Fit,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(top = 190.dp)
-                    .size(150.dp)
-                    .clip(RoundedCornerShape(16.dp)),
-            )
             Box {
                 Image(
-                    painter = painterResource(id = R.drawable.insta),
+                    painter = painterResource(id = R.drawable.shinjii),
                     contentDescription = null,
                     contentScale = ContentScale.Fit,
                     modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(top = 685.dp)
-                        .padding(end = 185.dp)
-                        .size(18.dp)
-                        .clip(RoundedCornerShape(16.dp))
+                        .padding(start = 115.dp)
+                        .padding(top = 200.dp).padding()
+                        .size(150.dp)
+                        .clip(RoundedCornerShape(80.dp))
+                        .border(3.dp, Color(113,31,140), CircleShape)
                 )
-            }
-            Box {
-                Image(
-                    painter = painterResource(id = R.drawable.zap),
-                    contentDescription = null,
-                    contentScale = ContentScale.Fit,
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(top = 652.dp)
-                        .padding(end = 185.dp)
-                        .size(18.dp)
-                        .clip(RoundedCornerShape(16.dp))
-                )
-            }
-            Box {
-                Image(
-                    painter = painterResource(id = R.drawable.gmail),
-                    contentDescription = null,
-                    contentScale = ContentScale.Fit,
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(top = 715.dp)
-                        .padding(end = 185.dp)
-                        .size(18.dp)
-                        .clip(RoundedCornerShape(16.dp))
-                )
-            }
-            Box {
                 Image(
                     painter = painterResource(id = R.drawable.eva),
                     contentDescription = null,
@@ -110,10 +73,8 @@ fun CartaoVisita() {
                         .padding(top = 50.dp)
                         .padding(start = 270.dp)
                         .size(190.dp)
-                        .align(Alignment.TopEnd)
                 )
             }
-            Box {
                 Image(
                     painter = painterResource(id = R.drawable.evaa),
                     contentDescription = null,
@@ -123,74 +84,62 @@ fun CartaoVisita() {
                         .padding(top = 450.dp)
                         .padding(end = 270.dp)
                         .size(190.dp)
-                        .align(Alignment.TopEnd)
                 )
-            }
             Column(
-                modifier = Modifier.fillMaxSize(),
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally
-
-
+                modifier = Modifier
+                    .padding(top = 350.dp)
+                    .fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally,
             ) {
 
                 Text(
                     text = "Raphael Rodrigues",
                     fontSize = 45.sp,
-                    fontWeight = FontWeight.Light,
-                    color = Color.White,
-                    textAlign = TextAlign.Center,
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(top = 322.dp)
+                    fontWeight = FontWeight.Medium,
+                    color = Color(44,140,31),
                 )
-
                 Text(
-                    text = "Android Developer Extraordinaire",
+                    text = "Android Developer Depressive",
                     fontSize = 15.sp,
                     fontWeight = FontWeight.Black,
-                    color = Color.White,
-                    textAlign = TextAlign.Center,
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(top = 0.dp)
+                    color = Color(44, 140,31),
                 )
-                Column(
-                    modifier = Modifier.fillMaxSize(),
-                    verticalArrangement = Arrangement.Center,
-                    horizontalAlignment = Alignment.CenterHorizontally
-
-                ) {
-                    Text(
-                        text = "   +55 (11)95206-2629 ",
-                        fontSize = 17.sp,
-                        fontWeight = FontWeight.Black,
-                        color = Color.White,
-                        textAlign = TextAlign.Center,
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(top = 150.dp)
-                    )
-                    Contatos(text = "@Rapha0riginal", Color.White)
-                    Contatos(text = "                    raphael.rb172@gmail.com", Color.White)
-                }
             }
-        }
+                Column(
+                    verticalArrangement = Arrangement.spacedBy(10.dp),
+                    modifier = Modifier
+                        .padding(start = 115.dp)
+                        .padding(top = 660.dp)
+                ) {
+                    Contatos(text = "+55 (11)95206-2629",
+                        id = R.drawable.zap)
+                    Contatos(text = "@Rapha0riginal",
+                        id = R.drawable.insta)
+                    Contatos(text = "raphael.rb172@gmail.com",
+                        id = R.drawable.gmail)
+                }
+
+
     }
 }
-
-    @Composable
-    fun Contatos(text: String, color: Color) {
-
+@Composable
+fun Contatos(text:String, id:Int) {
+    Row (
+        horizontalArrangement = Arrangement.spacedBy(10.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ){
+        Image(
+            painter = painterResource(id = id),
+            contentDescription = null,
+            modifier = Modifier
+                .size(25.dp)
+                .clip(RoundedCornerShape(16.dp))
+        )
         Text(
             text = text,
-            fontSize = 17.sp,
-            color = color,
-            fontWeight = FontWeight.Black,
-            textAlign = TextAlign.Center,
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(top = 10.dp)
-                .padding(end = 30.dp)
+            color = Color(44,140,31),
+            fontSize = 15.sp,
+            fontWeight = FontWeight.Black
         )
     }
+}
